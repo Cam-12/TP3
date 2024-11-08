3b4b6745ce490a8476f8d200160505d94cd9315d
Cam-12 <cameron.florence1@gmail.com>
2024-11-08 16:03:11 +0100

    v1

---
+++ exo2.py
@@ -0,0 +1,10 @@
+import time
+
+nombre= int(input("Entrez un nombre : "))
+
+if nombre>0:
+    for i in range(nombre, 0, -1):
+        time.sleep(1)
+        print(i)
+else:
+    print("Le nombre n'est pas positif")
