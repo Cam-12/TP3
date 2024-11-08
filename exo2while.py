a4a4b715804fe594fd10ad85764dbef645e94121
Cam-12 <cameron.florence1@gmail.com>
2024-11-08 16:13:20 +0100

    v1

--- exo2while.py
+++ exo2while.py
@@ -3,9 +3,9 @@ import time
 nombre= int(input("Entrez un nombre : "))
 
 if nombre>0:
-    while nombre!=1:
-        i= nombre-1
+    while nombre!=-1:
+        print(nombre)
+        nombre= nombre-1
         time.sleep(1)
-        print(i)
 else:
     print("Le nombre n'est pas positif")
